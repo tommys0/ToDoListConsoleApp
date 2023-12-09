8dc6db348d12395841a7f0d83d1613635086770a
tommys0 <149568540+tommys0@users.noreply.github.com>
2023-12-09 21:57:18 +0100

    Basic structure

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello \n" +
+                "This is my TO-DO List console app in Java \n" +
+                "It is one of my little projects just for fun and some experience.");
+    }
+}
